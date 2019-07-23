2d79a4bd66ae2673c7f86bbc6327bebdf45fe876
PetejStone <petejstoney@gmail.com>
2019-07-23 14:00:49 -0700

    installed auth0 and created Auth file

---
+++ src/Auth.js
@@ -0,0 +1,12 @@
+import auth0 from 'auth0-js'
+
+export default class Auth {
+    auth0 = new auth0.WebAuth({
+        domain: "dev-gco3gwsp.auth0.com",
+        clientID: "kFpGm0tbpc2lUax1Il5S0vS54opwh3iv",
+        redirectUri: "http://localhost:3000/callback",
+        audience: "http://localhost:3000/userinfo",
+        responseType: "token id_token",
+        scope: "openid"
+    })
+}

50fecf8a862f1b06d77d5f3e8a66653989d84013
PetejStone <petejstoney@gmail.com>
2019-07-24 15:23:23 -0700

    added dummy data and more stylings

--- src/RandomUserApi.js
+++ src/RandomUserApi.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+// import axios from "axios";
 
-export default  
-    axios
-      .get("https://randomuser.me/api/?results=4&inc=name,picture,email,registered")
-      .then( res => res)
+// export default  
+//     axios
+//       .get("https://randomuser.me/api/?results=4&inc=name,picture,email,registered")
+//       .then( res => res)
    
   
   
--- src/components/Owner/Profile.js
+++ src/components/Owner/Profile.js
@@ -9,29 +9,44 @@ class Profile extends React.Component {
             name: '',
             email: '',
             picture: '',
+            location: ''
         }
     }
     componentDidMount() {
        axios
-        .get("https://randomuser.me/api/?results=4&inc=name,picture,email,registered,location")
+        .get("https://randomuser.me/api/?nat=us&?results=1&inc=name,picture,email,registered,location")
         .then( res => this.setState({user: res.data.results[0]}))
        
     }
     
     render() {
         return (
-            <div>
-                <div>
-                 
+            <div className="profile-content">
+                <div className="user-info">
+                 {console.log(this.state.user.location.city)}
                     <img src={this.state.user.picture.large} alt=""/>
                     { this.state.user.name &&
-                    <p>{this.state.user.name.first.charAt(0).toUpperCase() +
+                    <p style={{fontWeight: 'bold'}}>{this.state.user.name.first.charAt(0).toUpperCase() +
                         this.state.user.name.first.slice(1) + ' ' + 
                         this.state.user.name.last.charAt(0).toUpperCase() +
                         this.state.user.name.last.slice(1) + ' '
                     }</p> }
-                    <p>Owner of Tech: Located in Queens, NY</p> 
+                    {this.state.user.location && 
+                    <p>Owner of Tech: Located in {
+                        this.state.user.location.city.charAt(0).toUpperCase() +
+                        this.state.user.location.city.slice(1) + ', ' + 
+                        this.state.user.location.state.charAt(0).toUpperCase() +
+                        this.state.user.location.state.slice(1) + ' '
+                    }</p>  }
+                    <p>Freelance Photographer</p>
                     <p> + Add Product</p>
+                    
+                </div>
+
+                <div className="products">
+                    <h3>Prod 1</h3>
+                    <h3>Prod 2</h3>
+                    <h3>Prod 3</h3>
                 </div>
             </div>
         )

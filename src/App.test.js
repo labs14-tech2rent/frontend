6c9aecbcaa0a6b846fe52837fe20d2681e0c5000
erik-sandoval <erik.sandoval@icloud.com>
2019-07-24 09:14:24 -0700

    added comments.

--- src/App.test.js
+++ src/App.test.js
@@ -5,11 +5,14 @@ import App from './App';
 
 import { findByTestAttr } from '../Utils/index';
 
+// enyzme needs a configuration to run. Have not figured out how to put
+// it in its own file.
 configure({
   adapter: new Adapter(),
   disableLifecycleMethods: true,
 });
 
+// refactor to call setup() instead of the whole component each time
 const setUp = (props = {}) => {
   const component1 = shallow(<App {...props} />);
   return component1;
@@ -20,6 +23,7 @@ beforeEach(() => {
   component = setUp();
 });
 
+// simple test to render the main app page.
 test('it should render', () => {
   const wrapper = findByTestAttr(component, '.App');
   expect(wrapper.length).toBe(1);
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,6 +2,8 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+// working on trying to call this instead of writing the adapter for each
+// test page.
 configure({
   adapter: new Adapter(),
   disableLifecycleMethods: true,

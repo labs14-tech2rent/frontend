40cc00cab28dd1a5814576795be28fd27355c046
PetejStone <petejstoney@gmail.com>
2019-07-26 15:20:22 -0700

    converted db check to array check rather than checking an object

--- src/Auth.js
+++ src/Auth.js
@@ -2,59 +2,79 @@
 import {addUser} from './actions';
 import auth0 from 'auth0-js'
 import axios from 'axios'
-const LOGIN_SUCCESS_PAGE = "/home"
+const LOGIN_EXISTS_PAGE = "/home"
 const LOGIN_FAILURE_PAGE = "/login"
 const LOGIN_REGISTER_PAGE = "/register"
 
 export default class Auth {
+    //Running a new auth0 call and pulling in the required domain and client id and other values needed for access
     auth0 = new auth0.WebAuth({
         domain: "dev-gco3gwsp.auth0.com",
         clientID: "kFpGm0tbpc2lUax1Il5S0vS54opwh3iv",
-        redirectUri: "http://localhost:3000/callback",
+        redirectUri: "hhttps://sharp-wozniak-279070.netlify.com/callback",
         responseType: "token id_token",
         audience: "https://dev-gco3gwsp.auth0.com/userinfo",
         scope: "openid"
     })
    
-      
+      // binds the login
     constructor() {
         this.login = this.login.bind(this);
     }
 
+    // Calls this fn when a user clicks login -- reroutes to a separate login page
     login() {
         this.auth0.authorize()
     }
 
     
-
+   //calls when user has logged in with auth0
     handleAuthentication( ) {
+        //parses the data to be read
         this.auth0.parseHash((err, authResults) => {
             console.log(authResults)
+            //if results are returned with an access token and an id token
             if (authResults && authResults.accessToken && authResults.idToken) {
+                //sets the expiration
                 let expiresAt = JSON.stringify((authResults.expiresIn) * 1000 + new Date().getTime())
+                //saving all required values to local storage to be referenced later for access to private route
                 localStorage.setItem("access_token", authResults.accessToken)
                 localStorage.setItem("id_token", authResults.idToken)
                 localStorage.setItem("expires_at", expiresAt)
                 localStorage.setItem('user_id', authResults.idTokenPayload.sub)
                 location.hash = ""
-                return axios.get('https://labstech2rentstaging.herokuapp.com/api/users/userIDS')
-                 .then(res => {
-                     res.data.map(res => {console.log(res.toString())})
-                    // if (Object.values(res.data).indexOf(authResults.idTokenPayload.sub) > -1) {
-                    //     console.log('exists')
-                    //     console.log(authResults.idTokenPayload.sub)
-                    //     console.log(res.data)
-                    //  } else {
-                    //     console.log('does not exist')
-                    //     console.log(authResults.idTokenPayload.sub)
-                    //     console.log(res.data)
-                    //  }
+
+                //create an empty array to check if user exists in OUR db (not auth0's)
+                const users = []
+                //make a call to our db to retrieve users
+                axios.get('https://labstech2rentstaging.herokuapp.com/api/users/userIDS')
+                 .then(res => { 
+                     //filter through data and pull out all the auth0_user_id values from their objects
+                     res.data.filter(res => {
+                        
+                        //grab all the above values and push them inside users array
+                        users.push(res.auth0_user_id)
+                        
+                       
+                     })
+                   
+                   //check against our db to see if it includes the userId that was just logged in
+                    if (users.includes(authResults.idTokenPayload.sub)) {
+                        console.log('exists')
+                        //if they exists in our db, then reroute them to the home page
+                        location.pathname = LOGIN_EXISTS_PAGE
+                     } else {
+                        console.log('does not exist')
+                        //if they do not exist reroute them to finish registration
+                        location.pathname = LOGIN_REGISTER_PAGE
+                     }
                     
 
                 })
                 .catch(err => console.log(err))       
             } else if (err) {
-               // location.pathname = LOGIN_FAILURE_PAGE
+                //if failure, reroute to the login page to try again
+               location.pathname = LOGIN_FAILURE_PAGE
                 console.log(err)
             }
         })
@@ -63,6 +83,7 @@ export default class Auth {
     isAuthenticated() {
         console.log('checking auth')
         let expiresAt = JSON.parse(localStorage.getItem("expires_at"))
+        //if the new date is less that the expiration date, then return
         return new Date().getTime < expiresAt
     }
 }

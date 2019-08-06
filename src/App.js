65f533ce4b53f5c01f2d8e51e79e61e8409124db
erik-sandoval <erik.sandoval@icloud.com>
2019-08-06 13:37:17 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,20 +9,26 @@ import PrivateRoute from './PrivateRoute';
 import Profile from './components/Owner/Profile';
 import Register from './components/Register/Register';
 
-// import NavBar from './components/Nav/NavBar';
+import NavBar from './components/Nav/NavBar';
 import Footer from './components/Footer/Footer';
 import CreateListing from './components/CreateListing/CreateListing';
-// import auth from './Auth'
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <BrowserRouter>
-          <header>{/* <NavBar {...this.props} /> */}</header>
+          <header>
+            <NavBar {...this.props} />
+          </header>
           <Switch>
             <Route exact path="/" {...this.props} component={Login} />
             <Route exact path="/callback" component={Callback} />
-            <PrivateRoute auth={this.props.submit} path="/home" component={HomePage} />
+            <PrivateRoute
+              auth={this.props.submit}
+              path="/home"
+              component={HomePage}
+            />
             <PrivateRoute exact path="/profile" component={Profile} />
             <PrivateRoute path="/create-listing" component={CreateListing} />
             <Route exact path="/register" component={Register} />

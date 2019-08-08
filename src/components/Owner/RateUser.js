befe19a3e0be8c497fbe982d7971443902640ddc
PetejStone <petejstoney@gmail.com>
2019-08-08 16:39:03 -0700

    search-in-navbar

---
+++ src/components/Owner/RateUser.js
@@ -0,0 +1,24 @@
+
+
+//document.querySelector('.rating') && const rating = document.querySelector('.rating') 
+function RateUser(props) {
+    window.addEventListener('DOMContentLoaded', (event) => {
+        const rating = document.querySelector('.rating').children
+
+      console.log(rating.length)
+       
+      for (let i=0; i < rating.length; i++) {
+          rating[i].addEventListener('click', (e) => {
+          
+          e.target.classList.toggle('selected')
+          
+          
+          })
+      }
+        
+     });
+   
+
+}
+
+export default RateUser;

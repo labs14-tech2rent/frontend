861ca3f394d8f9ac517e0e00b0ae228a434d3b27
erik-sandoval <erik.sandoval@icloud.com>
2019-08-14 17:01:44 -0700

    added testing file for actions.

--- src/components/TestingActions.js
+++ src/components/TestingActions.js
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { getListings } from '../actions/Listings/getListings';
+import { getListings } from '../actions/Items/CRUD/getItems';
+import { getUserItems } from '../actions/Items/CRUD/getUserItems';
+import { getItemById } from '../actions/Items/CRUD/getItemById';
 
 const TestingActions = () => {
   const dispatch = useDispatch();
-  const hello = 'hi';
 
   useEffect(() => {
-    dispatch(getListings);
+    dispatch(getListings());
+    dispatch(getUserItems(27));
+    dispatch(getItemById(1));
   });
+
+
 
   return <div>Hello</div>;
 };

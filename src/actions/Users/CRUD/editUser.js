f7e452d54026b42507c93b86dfaf4e4a9d96ef7b
erik-sandoval <erik.sandoval@icloud.com>
2019-08-14 20:46:38 -0700

    Update editUser.js

--- src/actions/Users/CRUD/editUser.js
+++ src/actions/Users/CRUD/editUser.js
@@ -7,7 +7,7 @@ export const EDIT_USER_FAIL = 'EDIT_USER_FAIL;';
 export const editUser = (id, details) => dispatch => {
   dispatch({ type: EDIT_USER_START });
   return axios
-    .post(`https://labstech2rent.herokuapp.com/api/users/${id}`, details)
+    .put(`https://labstech2rent.herokuapp.com/api/users/${id}`, details)
     .then(res => {
       dispatch({ type: EDIT_USER_SUCCESS, payload: res }); // /
     })

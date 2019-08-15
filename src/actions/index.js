8c260ba73931a8aca076484a36651619a55b6a9b
erik-sandoval <erik.sandoval@icloud.com>
2019-08-14 20:14:40 -0700

    exported actions from index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+// User Actions
 export {
   ADD_USER_START,
   ADD_USER_FAIL,
@@ -20,15 +21,80 @@ export {
 } from './Users/randomUsers';
 
 export {
-  CREATE_LISTING_FAILED,
-  CREATE_LISTING_START,
-  CREATE_LISTING_SUCCESS,
-  createItem,
-} from './Items/CRUD/createItem';
-
-export {
   GET_USER_START,
   GET_USER_SUCCESS,
   GET_USER_FAIL,
   getUserId,
 } from './Users/getUserId';
+
+// Item CRUD Methods
+export {
+  GET_USER_ITEMS_START,
+  GET_USER_ITEMS_SUCCESS,
+  GET_USER_ITEMS_FAIL,
+  getUserItems,
+} from './Items/CRUD/getUserItems';
+
+export {
+  GET_ITEM_BY_ID_START,
+  GET_ITEM_BY_ID_SUCCESS,
+  GET_ITEM_BY_ID_FAIL,
+  getItemById,
+} from './Items/CRUD/getItemById';
+
+export {
+  GET_ITEMS_START,
+  GET_ITEMS_SUCCESS,
+  GET_ITEMS_FAIL,
+  getItems,
+} from './Items/CRUD/getItems';
+
+export {
+  CREATE_LISTING_START,
+  CREATE_LISTING_SUCCESS,
+  CREATE_LISTING_FAILED,
+  createItem,
+} from './Items/CRUD/createItem';
+
+export {
+  EDIT_ITEM_START,
+  EDIT_ITEM_SUCCESS,
+  EDIT_ITEM_FAIL,
+  editItem,
+} from './Items/CRUD/editItem';
+
+export {
+  DELETE_ITEM_START,
+  DELETE_ITEM_SUCCESS,
+  DELETE_ITEM_FAIL,
+  deleteItem,
+} from './Items/CRUD/deleteItem';
+
+// Search Actions
+export {
+  SEARCH_CAT_START,
+  SEARCH_CAT_SUCCESS,
+  SEARCH_CAT_FAIL,
+  searchCategory,
+} from './Items/Search/searchCategory';
+
+export {
+  SEARCH_CITY_START,
+  SEARCH_CITY_SUCCESS,
+  SEARCH_CITY_FAIL,
+  searchCity,
+} from './Items/Search/searchCity';
+
+export {
+  SEARCH_CONDITION_START,
+  SEARCH_CONDITION_SUCCESS,
+  SEARCH_CONDITION_FAIL,
+  searchCondition,
+} from './Items/Search/searchCondition';
+
+export {
+  SEARCH_ZIPCODE_START,
+  SEARCH_ZIPCODE_SUCCESS,
+  SEARCH_ZIPCODE_FAIL,
+  searchZipcode,
+} from './Items/Search/searchZipcode';

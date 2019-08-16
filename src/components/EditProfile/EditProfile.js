63ccd90074990c38edd8d0f886b0808acc85de33
PetejStone <petejstoney@gmail.com>
2019-08-16 13:22:33 -0700

    testing

--- src/components/EditProfile/EditProfile.js
+++ src/components/EditProfile/EditProfile.js
@@ -55,6 +55,71 @@ const EditProfile = props => {
             alt=""
           />
         </div>
+        {editName ? (
+          <form>
+            <input
+              type="text"
+              onChange={handleChange}
+              name="name"
+              value={userInfo.name}
+            />
+            <button
+              onClick={e => {
+                e.preventDefault();
+                setEditName(!editName);
+              }}
+            >
+              Update Name
+            </button>
+          </form>
+        ) : (
+          <div className="edit-content-container">
+            <p className="profile-edit-text">{userInfo.name}</p>
+            <button
+              onClick={() => {
+                setEditName(!editName);
+              }}
+            >
+              Edit Name
+            </button>
+          </div>
+        )}
+        {editLocation ? (
+          <form onSubmit={editLocation}>
+            <input
+              type="text"
+              onChange={handleChange}
+              name="city"
+              placeholder={userInfo.city}
+              value={userInfo.location}
+            />
+
+            <StateSelect state={userInfo.state} handleState={handleState} />
+
+            <input
+              type="text"
+              onChange={handleChange}
+              name="zip"
+              placeholder={userInfo.zip}
+              value={userInfo.zip}
+            />
+            <button
+              onClick={e => {
+                e.preventDefault();
+                setEditLocation(!editLocation);
+              }}
+            >
+              Update Location
+            </button>
+          </form>
+        ) : (
+          <div className="edit-content-container">
+            <p className="profile-edit-text">{`${userInfo.city} ${userInfo.state}, ${userInfo.zip}`}</p>
+            <button onClick={() => setEditLocation(!editLocation)}>
+              Edit Location
+            </button>
+          </div>
+        )}
 
         <Rating />
       </div>

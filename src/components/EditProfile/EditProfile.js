12a153531be61281d35202a0824e1d40df2b8c1d
PetejStone <petejstoney@gmail.com>
2019-08-16 16:33:19 -0700

    testing

--- src/components/EditProfile/EditProfile.js
+++ src/components/EditProfile/EditProfile.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
 import { useDispatch, useSelector } from 'react-redux';
 import StateSelect from './StateSelect';
 import Rating from '../Owner/Rating';

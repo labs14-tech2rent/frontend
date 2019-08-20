30adc433de00afbaa138c29fc51b9928f9111694
PetejStone <petejstoney@gmail.com>
2019-08-20 14:41:18 -0700

    pulled from master

--- src/Logout.js
+++ src/Logout.js
@@ -1,4 +1,4 @@
-// Script uses auth0.js. See Remarks for details.
+// / Script uses auth0.js. See Remarks for details.
 import auth0 from 'auth0-js';
 
 export default () => {
@@ -8,7 +8,7 @@ export default () => {
   });
 
   webAuth.logout({
-    //returnTo: 'http://localhost:3000/v2/logout',
+    // returnTo: 'http://localhost:3000/v2/logout',
     // returnTo: 'https://tech2rent.co/v2/logout',
     returnTo: 'https://sharp-wozniak-279070.netlify.com/v2/logout',
     client_id: 'kFpGm0tbpc2lUax1Il5S0vS54opwh3iv',

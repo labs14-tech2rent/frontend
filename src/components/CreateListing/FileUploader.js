d5fbb1eb212381d5e8e0b78502415a75cf48ebfc
Erik Sandoval <erik.sandoval@icloud.com>
2019-08-21 10:58:43 -0700

    removed the upload functionality from file uploader.

--- src/components/CreateListing/FileUploader.js
+++ src/components/CreateListing/FileUploader.js
@@ -4,29 +4,6 @@ import Dropzone from 'react-dropzone-uploader';
 import 'react-dropzone-uploader/dist/styles.css';
 
 const FileUpload = props => {
-  // const getUploadParams = ({ file }) => {
-  //   const randomNum = `${Math.floor(
-  //     Math.random() * 100000000000000 + 1
-  //   )}-${Math.floor(Math.random() * 10000000 + 1)}`;
-  //   const body = new FormData();
-  //   body.append('name', file, randomNum);
-  //   const url =
-  //     // 'https://labstech2rentstaging.herokuapp.com/api/items/uploadProfilePicture';
-  //     'https://httpbin.org/post';
-
-  //   const photo = {
-  //     body,
-  //     url,
-  //     meta: {
-  // fileUrl: `https://labs14-tech2rent-image-upload.s3.amazonaws.com/${encodeURIComponent(
-  //   randomNum
-  // )}`,
-  //     },
-  //   };
-
-  //   return photo;
-  // };
-
   const handleChangeStatus = ({ file, meta }, status) => {
     console.log(file);
     console.log(status, meta);

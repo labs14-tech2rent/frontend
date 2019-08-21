aa63d8f996c1ee1875593e7b85b6fe78c25ad192
Erik Sandoval <erik.sandoval@icloud.com>
2019-08-20 17:18:26 -0700

    Right now not using getuploadparams to test out somewhere else.

--- src/components/CreateListing/FileUploader.js
+++ src/components/CreateListing/FileUploader.js
@@ -1,39 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Dropzone from 'react-dropzone-uploader';
 
 import 'react-dropzone-uploader/dist/styles.css';
 
 const FileUpload = props => {
-  const [picture, setPicture] = useState([]);
+  // const getUploadParams = ({ file }) => {
+  //   const randomNum = `${Math.floor(
+  //     Math.random() * 100000000000000 + 1
+  //   )}-${Math.floor(Math.random() * 10000000 + 1)}`;
+  //   const body = new FormData();
+  //   body.append('name', file, randomNum);
+  //   const url =
+  //     // 'https://labstech2rentstaging.herokuapp.com/api/items/uploadProfilePicture';
+  //     'https://httpbin.org/post';
 
-  const getUploadParams = ({ file, meta }) => {
-    const body = new FormData();
-    body.append('name', file);
-    const url =
-      'https://labstech2rentstaging.herokuapp.com/api/items/uploadProfilePicture';
-    return {
-      body,
-      url,
-      meta: {
-        fileUrl: `https://labs14-tech2rent-image-upload.s3.amazonaws.com/${encodeURIComponent(
-          meta.name
-        )}`,
-      },
-    };
-  };
+  //   const photo = {
+  //     body,
+  //     url,
+  //     meta: {
+  // fileUrl: `https://labs14-tech2rent-image-upload.s3.amazonaws.com/${encodeURIComponent(
+  //   randomNum
+  // )}`,
+  //     },
+  //   };
 
-  const handleChangeStatus = ({ meta }, status) => {
-    // meta.name = `${Math.floor(Math.random() * 100000000000 + 1)}-${Math.floor(
-    //   Math.random() * 10000 + 1
-    // )}`;
+  //   return photo;
+  // };
+
+  const handleChangeStatus = ({ file, meta }, status) => {
+    console.log(file);
     console.log(status, meta);
+
+    const randomNum = `${Math.floor(
+      Math.random() * 100000000000000 + 1
+    )}-${Math.floor(Math.random() * 10000000 + 1)}`;
+
+    const body = new FormData();
+
+    body.append('name', file, randomNum);
+
+    if (status === 'done') {
+      props.savePhotos({
+        body,
+        file,
+      });
+    }
+
+    if (status === 'removed') {
+      console.log(file);
+    }
   };
 
-  console.log(picture);
   return (
     <Dropzone
       onChangeStatus={handleChangeStatus}
-      getUploadParams={getUploadParams}
+      // getUploadParams={getUploadParams}
       maxFiles={6}
       inputContent="Upload Image(s)"
       styles={{ dropzone: { minHeight: 250, maxHeight: 250 } }}

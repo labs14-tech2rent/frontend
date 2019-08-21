cc3978502d6f24acb88849eab76aa5fac0feb792
Erik Sandoval <erik.sandoval@icloud.com>
2019-08-20 17:40:33 -0700

    removed console.log's

--- src/components/CreateListing/Form.js
+++ src/components/CreateListing/Form.js
@@ -25,7 +25,6 @@ const Form = props => {
           photo.body
         )
         .then(res => {
-          console.log(res);
           userPhotos.push({ url: res.data.Location });
           setPicture([...picture, userPhotos]);
         })
@@ -34,8 +33,6 @@ const Form = props => {
         })
     );
   };
-
-  console.log(picture);
 
   return (
     <Formik
